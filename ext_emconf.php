2a58bd0265ebd9d378edd4e796b73e34a6524bf5
elmar.hinz <elmar.hinz@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-07-12 09:55:11 +0000

    initial import
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ect/trunk@5924 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,53 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "ect"
+#
+# Auto generated 11-07-2007 14:58
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'ECT coordinated extensions',
+	'description' => 'Declares dependencies from libraries and basic extensions coordinated within the Extension Coordination Team. NG:typo3.teams.extension-coordination',
+	'category' => 'misc',
+	'author' => 'Extension Coordination Team',
+	'author_email' => 'nntp://typo3.team.extension-coordination',
+	'shy' => '',
+	'dependencies' => 'pear,div,lib,xajax,jquery,extjs,partner,cal,kickstarter__mvc',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => 'Extension Coordination Team',
+	'version' => '0.1.0',
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"4e1a";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"b4e6";s:19:"doc/wizard_form.dat";s:4:"da09";s:20:"doc/wizard_form.html";s:4:"5e6c";}',
+	'constraints' => array(
+		'depends' => array(
+			'pear' => '',
+			'div' => '',
+			'lib' => '',
+			'xajax' => '',
+			'jquery' => '',
+			'extjs' => '',
+			'partner' => '',
+			'cal' => '',
+			'kickstarter__mvc' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
+
+?>
